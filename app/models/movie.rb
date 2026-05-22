11a316f2cd47d811012e3639ba43671b170e1b7b
Laure Valmier <lvalmier@gmail.com>
2026-05-22 11:27:21 +0200

    Add seed with 4 movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks
   has_many :lists, through: :bookmarks
+  has_many :bookmarks
 
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
